f8d3c18c8ce57f95a7a95895306ee51b82936bdd
LLCampos <luis1212@sapo.pt>
2016-05-29 16:55:52 +0100

    update documentation

--- public_html/webservices/doc/api_project.js
+++ public_html/webservices/doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-05-16T17:15:49.258Z",
+    "time": "2016-05-29T15:54:14.076Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
